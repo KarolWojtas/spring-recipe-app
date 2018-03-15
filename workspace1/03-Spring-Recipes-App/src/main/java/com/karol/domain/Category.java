8d864d1473269ba5aef70ea6e9e71b8e292d7840
KarolWojtas <karol.wojtas.aim@gmail.com>
2018-03-15 18:23:28 +0100

    damn

---
+++ workspace1/03-Spring-Recipes-App/src/main/java/com/karol/domain/Category.java
@@ -0,0 +1,50 @@
+package com.karol.domain;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+@Entity
+@Data
+@ToString(exclude = "recipes")
+@EqualsAndHashCode(exclude = "recipes")
+public class Category {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="category_id")
+	private Long id;
+	private String description;
+	@ManyToMany(mappedBy="categories")
+	private Set<Recipe> recipes = new HashSet<>();
+	/*public Category() {
+		// TODO Auto-generated constructor stub
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Set<Recipe> getRecipes() {
+		return recipes;
+	}
+	public void setRecipes(Set<Recipe> recipes) {
+		this.recipes = recipes;
+	}
+*/
+}
